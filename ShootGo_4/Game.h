2261f6e04af166aa019575d0901f7eab5f3db880
Mohammad MDSA <samimd.77@gmail.com>
2017-08-26 15:49:01 +0430

    Render a simple triangle

---
+++ ShootGo_4/Game.h
@@ -0,0 +1,85 @@
+//
+// Game.h
+//
+
+#pragma once
+
+#include "StepTimer.h"
+
+#include "CommonStates.h"
+#include "DDSTextureLoader.h"
+#include "DirectXHelpers.h"
+#include "Effects.h"
+#include "GamePad.h"
+#include "GeometricPrimitive.h"
+#include "Model.h"
+#include "PrimitiveBatch.h"
+#include "ScreenGrab.h"
+#include "SimpleMath.h"
+#include "SpriteBatch.h"
+#include "SpriteFont.h"
+#include "VertexTypes.h"
+#include "WICTextureLoader.h"
+
+using namespace std;
+using namespace DirectX;
+
+
+// A basic game implementation that creates a D3D11 device and
+// provides a game loop.
+class Game
+{
+public:
+
+    Game();
+
+    // Initialization and management
+    void Initialize(HWND window, int width, int height);
+
+    // Basic game loop
+    void Tick();
+
+    // Messages
+    void OnActivated();
+    void OnDeactivated();
+    void OnSuspending();
+    void OnResuming();
+    void OnWindowSizeChanged(int width, int height);
+
+    // Properties
+    void GetDefaultSize( int& width, int& height ) const;
+
+private:
+
+    void Update(DX::StepTimer const& timer);
+    void Render();
+
+    void Clear();
+    void Present();
+
+    void CreateDevice();
+    void CreateResources();
+
+    void OnDeviceLost();
+
+    // Device resources.
+    HWND                                            m_window;
+    int                                             m_outputWidth;
+    int                                             m_outputHeight;
+
+    D3D_FEATURE_LEVEL                               m_featureLevel;
+    Microsoft::WRL::ComPtr<ID3D11Device1>           m_d3dDevice;
+    Microsoft::WRL::ComPtr<ID3D11DeviceContext1>    m_d3dContext;
+
+    Microsoft::WRL::ComPtr<IDXGISwapChain1>         m_swapChain;
+    Microsoft::WRL::ComPtr<ID3D11RenderTargetView>  m_renderTargetView;
+    Microsoft::WRL::ComPtr<ID3D11DepthStencilView>  m_depthStencilView;
+
+    // Rendering loop timer.
+    DX::StepTimer                                   m_timer;
+
+	unique_ptr<CommonStates>                        m_states;
+	unique_ptr<BasicEffect>                         m_effect;
+	unique_ptr<PrimitiveBatch<VertexPositionColor>> m_batch;
+	Microsoft::WRL::ComPtr<ID3D11InputLayout>       m_inputLayout;
+};
